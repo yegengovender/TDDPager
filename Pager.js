6105d5f842d0c144a91e54f775b71d831f50fac6
yegengovender <yegengovender@gmail.com>
2014-05-22 10:23:09 +0200

    first commit

---
+++ Pager.js
@@ -0,0 +1,34 @@
+var Pager = function (data, itemsPerPage) {
+    var _data = data;
+    var _itemsPerPage = itemsPerPage;
+    var numberOfPages = parseInt(_data.length / _itemsPerPage);
+
+    if (_data.length % _itemsPerPage > 0) {
+        numberOfPages++;
+    }
+
+    var _pages = [];
+
+    for (var page = 1; page <= numberOfPages; page++) {
+        var pageStart = (page-1) * _itemsPerPage;
+        var pageEnd = pageStart + _itemsPerPage;
+        _pages[page] = _data.slice(pageStart, pageEnd);
+    }
+
+    this.pages = _pages;
+    this.currentPageNumber = 1;
+
+    this.goToPage = function (pageNumber) {
+        if (_pages[pageNumber]) {
+            this.currentPageNumber = pageNumber;
+        }
+    }
+
+    this.nextPage = function () {
+        this.goToPage(this.currentPageNumber + 1);
+    };
+
+    this.previousPage = function() {
+        this.goToPage(this.currentPageNumber - 1);
+    };
+};
---
+++ Pager.spec.js
@@ -0,0 +1,76 @@
+var list = [
+    'object1',
+    'object2',
+    'object3',
+    'object4',
+    'object5',
+    'object6',
+    'object7',
+    'object8'
+];
+
+var pager;
+
+describe("Pager", function () {
+    it("accepts a list of objects to page, and number of items per page", function () {
+        pager = new Pager(list, 4);
+        expect(pager).toBeTruthy();
+    });
+
+    it("number of pages for list is length of list divided by number of items per page, and an extra page for remainder", function () {
+        pager = new Pager(list, 3);
+        expect(pager.pages.length).toEqual(4);
+        pager = new Pager(list, 4);
+        expect(pager.pages.length).toEqual(3); // array with indexes starting at 1 has length more than count of values
+    });
+
+    it("divides list into data for each page", function () {
+        var page1 = list.slice(0, 3),
+            page2 = list.slice(3, 6),
+            page3 = list.slice(6, 8);
+        pager = new Pager(list, 3);
+        expect(pager.pages[1]).toEqual(page1);
+        expect(pager.pages[2]).toEqual(page2);
+        expect(pager.pages[3]).toEqual(page3);
+    });
+
+    it("initially sets current page to 1", function() {
+        pager = new Pager(list, 3);
+        expect(pager.currentPageNumber).toEqual(1);
+    });
+
+    it("nextPage will increment currentPageNumber except on last page", function() {
+        pager = new Pager(list, 3);
+        pager.nextPage();
+        expect(pager.currentPageNumber).toEqual(2);
+        pager.nextPage();
+        expect(pager.currentPageNumber).toEqual(3);
+        pager.nextPage();
+        expect(pager.currentPageNumber).toEqual(3);
+    });
+
+    it("goToPage will change currentPageNumber if that page exists", function() {
+        pager = new Pager(list, 3);
+        pager.goToPage(3);
+        expect(pager.currentPageNumber).toEqual(3);
+        pager.goToPage(4);
+        expect(pager.currentPageNumber).toEqual(3);
+        pager.goToPage(-1);
+        expect(pager.currentPageNumber).toEqual(3);
+        pager.goToPage(2);
+        expect(pager.currentPageNumber).toEqual(2);
+    });
+
+    it("previousPage will decrement currentPageNumber if previous page exists", function () {
+        pager = new Pager(list, 3);
+        pager.previousPage();
+        expect(pager.currentPageNumber).toEqual(1);
+        pager.goToPage(3);
+        pager.previousPage();
+        expect(pager.currentPageNumber).toEqual(2);
+        pager.previousPage();
+        expect(pager.currentPageNumber).toEqual(1);
+        pager.previousPage();
+        expect(pager.currentPageNumber).toEqual(1);
+    });
+});
