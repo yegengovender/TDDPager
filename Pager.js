89463667c27b8d9a7302e48877cb4023bfdc0323
yegengovender <yegengovender@gmail.com>
2014-05-22 15:10:38 +0200

    added page ranges

--- Pager.js
+++ Pager.js
@@ -1,6 +1,8 @@
-var Pager = function (data, itemsPerPage) {
+var Pager = function (data, itemsPerPage, range) {
     var _data = data;
     var _itemsPerPage = itemsPerPage;
+    var _range = range;
+
     var numberOfPages = parseInt(_data.length / _itemsPerPage);
 
     if (_data.length % _itemsPerPage > 0) {
@@ -8,16 +10,45 @@ var Pager = function (data, itemsPerPage) {
     }
 
     var _pages = [];
+    var _pageNumbers = [];
 
     for (var page = 1; page <= numberOfPages; page++) {
-        var pageStart = (page-1) * _itemsPerPage;
+        var pageStart = (page - 1) * _itemsPerPage;
         var pageEnd = pageStart + _itemsPerPage;
         _pages[page] = _data.slice(pageStart, pageEnd);
+        _pageNumbers.push(page);
     }
+
 
     this.pages = _pages;
     this.currentPageNumber = 1;
     this.lastPageNumber = _pages.length - 1;
+    this.pageNumbers = _pageNumbers;
+    this.visiblePageNumbers = function() {
+        if (_range && _range < this.pageNumbers.length) {
+            var _visiblePageNumbers = [];
+            var minPage;
+            var maxPage;
+
+            minPage = this.currentPageNumber - parseInt(_range / 2);
+            if (minPage < 1) {
+                minPage = 1;
+            }
+
+            maxPage = minPage + _range - 1;
+            if (maxPage > this.lastPageNumber) {
+                maxPage = this.lastPageNumber;
+                minPage = maxPage - _range + 1;
+            } 
+
+            for (var page = minPage; page <= maxPage; page++) {
+                _visiblePageNumbers.push(page);
+            }
+        } else {
+            _visiblePageNumbers = _pageNumbers;
+        }
+        return _visiblePageNumbers;
+    }
 
     this.goToPage = function (pageNumber) {
         if (_pages[pageNumber]) {
@@ -29,7 +60,7 @@ var Pager = function (data, itemsPerPage) {
         this.goToPage(this.currentPageNumber + 1);
     };
 
-    this.previousPage = function() {
+    this.previousPage = function () {
         this.goToPage(this.currentPageNumber - 1);
     };
 };
--- Pager.spec.js
+++ Pager.spec.js
@@ -9,6 +9,49 @@ var list = [
     'object8'
 ];
 
+var longlist = [
+    'object1',
+    'object2',
+    'object3',
+    'object4',
+    'object5',
+    'object6',
+    'object7',
+    'object8',
+    'object9',
+    'object10',
+    'object11',
+    'object12',
+    'object13',
+    'object14',
+    'object15',
+    'object16',
+    'object17',
+    'object18',
+    'object19',
+    'object20',
+    'object21',
+    'object22',
+    'object23',
+    'object24',
+    'object25',
+    'object26',
+    'object27',
+    'object28',
+    'object29',
+    'object30',
+    'object31',
+    'object32',
+    'object33',
+    'object34',
+    'object35',
+    'object36',
+    'object37',
+    'object38',
+    'object39',
+    'object40',
+];
+
 var pager;
 
 describe("Pager", function () {
@@ -34,12 +77,12 @@ describe("Pager", function () {
         expect(pager.pages[3]).toEqual(page3);
     });
 
-    it("initially sets current page to 1", function() {
+    it("initially sets current page to 1", function () {
         pager = new Pager(list, 3);
         expect(pager.currentPageNumber).toEqual(1);
     });
 
-    it("nextPage will increment currentPageNumber except on last page", function() {
+    it("nextPage will increment currentPageNumber except on last page", function () {
         pager = new Pager(list, 3);
         pager.nextPage();
         expect(pager.currentPageNumber).toEqual(2);
@@ -49,7 +92,7 @@ describe("Pager", function () {
         expect(pager.currentPageNumber).toEqual(3);
     });
 
-    it("goToPage will change currentPageNumber if that page exists", function() {
+    it("goToPage will change currentPageNumber if that page exists", function () {
         pager = new Pager(list, 3);
         pager.goToPage(3);
         expect(pager.currentPageNumber).toEqual(3);
@@ -73,7 +116,7 @@ describe("Pager", function () {
         pager.previousPage();
         expect(pager.currentPageNumber).toEqual(1);
     });
-    
+
     it("lastPage will return last page number available", function () {
         pager = new Pager(list, 3);
         expect(pager.lastPageNumber).toEqual(3);
@@ -81,4 +124,46 @@ describe("Pager", function () {
         expect(pager.lastPageNumber).toEqual(2);
     });
 
+    it("has the list of page numbers", function () {
+        pager = new Pager(list, 3);
+        expect(pager.pageNumbers).toEqual([1, 2, 3]);
+    });
+
+    it("visible page numbers are all page numbers if range is not specified", function () {
+        pager = new Pager(longlist, 6);
+        expect(pager.visiblePageNumbers()).toEqual([1, 2, 3, 4, 5, 6, 7]);
+    });
+
+    it("visible page numbers count is specified by range parameter", function () {
+        pager = new Pager(longlist, 6, 4);
+        expect(pager.visiblePageNumbers()).toEqual([1, 2, 3, 4]);
+    });
+
+    it("visible page numbers count is first few pages when on first page", function () {
+        pager = new Pager(longlist, 6, 4);
+        expect(pager.visiblePageNumbers()).toEqual([1, 2, 3, 4]);
+    });
+
+    it("visible page numbers count is last few pages when on last page", function () {
+        pager = new Pager(longlist, 6, 4);
+        pager.goToPage(pager.lastPageNumber);
+        expect(pager.visiblePageNumbers()).toEqual([4, 5, 6, 7]);
+    });
+
+    it("visible page numbers count is not more than page numbers", function () {
+        pager = new Pager(longlist, 10, 5);
+        pager.goToPage(pager.lastPageNumber);
+        expect(pager.visiblePageNumbers()).toEqual([1, 2, 3, 4]);
+    });
+
+    it("visible page numbers is range amount with current page in the 'middle'", function () {
+        pager = new Pager(longlist, 6, 4);
+        pager.goToPage(4);
+        expect(pager.visiblePageNumbers()).toEqual([2, 3, 4, 5]);
+        pager.goToPage(5);
+        expect(pager.visiblePageNumbers()).toEqual([3, 4, 5, 6]);
+        pager = new Pager(longlist, 8, 3);
+        pager.goToPage(3);
+        expect(pager.visiblePageNumbers()).toEqual([2, 3, 4]);
+    });
 });
